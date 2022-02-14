76aebcf61a784bb2f167ecc222314338b157a417
Hwessels <Hwessels@student.wethinkcode.co.za>
2022-02-14 21:34:54 +0200

    added Python Arithmetic Operations and Division submissions

---
+++ Submissions/Practice/Python/01. Introduction/003. Arithmetic Operators/answer.py
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+# Score = 10
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print( str( a + b ) )
+    print( str( a - b ) )
+    print( str( a * b ) )
